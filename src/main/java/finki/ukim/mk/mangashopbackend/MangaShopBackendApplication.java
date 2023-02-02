ff43a0d0f50faf4e363c153f8e4380375a3a87df
Andrej Mishevski <andrej.mishevski@students.finki.ukim.mk>
2023-02-02 19:50:23 +0100

    Initial commit

---
+++ src/main/java/finki/ukim/mk/mangashopbackend/MangaShopBackendApplication.java
@@ -0,0 +1,13 @@
+package finki.ukim.mk.mangashopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangaShopBackendApplication.class, args);
+    }
+
+}
